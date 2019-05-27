01d88560b185f56bea734a6c3f0e0c280e1fa32d
Xiaohui Liu <jayliu925@gmail.com>
2019-05-27 01:33:47 -0600

    very basic version, need APIs

--- frontEndLibraries/randomQuoteMachine/index.js
+++ frontEndLibraries/randomQuoteMachine/index.js
@@ -1,8 +1,28 @@
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c',
-    '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"
+var colors = ['#16a085', '#27ae60',
+    '#2c3e50', '#f39c12',
+    '#e74c3c', '#9b59b6',
+    '#FB6964', '#342224',
+    "#472E32", "#BDBB99",
+    "#77B1A9", "#73A857"
 ];
 
-$(document).ready(function () {
-    $("#new-quote").click()
-});
+var quotes = [
+    "11111",
+    "22222",
+    "33333",
+    "44444"
+]
 
+/* Generating new quote */
+$("#new-quote").click(renderNewQuote)
+
+function renderNewQuote() {
+    randQuote = getRandQuote()
+    $("#text").html(randQuote)
+}
+
+function getRandQuote() {
+    randIndex = Math.floor(Math.random() * quotes.length)
+    randQuote = quotes[randIndex]
+    return randQuote
+}
